9a25d1d1c9418dadf15758066ba51c4f81681cc2
Siripaak Chinnachot <siripaakc@gmail.com>
2021-03-07 17:50:02 +0700

    edited

--- bankingFeature/banking.go
+++ bankingFeature/banking.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"math/rand"
 	"os"
@@ -8,90 +9,37 @@ import (
 )
 
 var listAccount []accountDetail
+var phone string
 
 func main() {
-	var password string
-	fmt.Println("Password for admin is admin")
-	fmt.Println("Password for user is user")
-	fmt.Printf("Enter your password : ")
-	fmt.Scan(&password)
-	if password == "user" {
-		fmt.Printf("Password match!\nLoading")
-		for i := 0; i < 5; i++ {
-			// time.Sleep(1 * time.Second)
-			fmt.Printf(".")
-		}
-		fmt.Println("")
-		mainMenuUser()
-	} else if password == "admin" {
-		fmt.Printf("Password match!\nLoading")
-		for i := 0; i < 5; i++ {
-			time.Sleep(1 / 2 * time.Second)
-			fmt.Printf(".")
-		}
-		fmt.Println("")
-		mainMenuAdmin()
-	} else {
-		exitProgram()
-	}
-}
-
-func mainMenuUser() {
 	var selection int
-	fmt.Println("-------- User Main Menu -----------")
-	fmt.Printf("1.Create Account\n")
-	fmt.Printf("2.Transaction\n")
-	fmt.Printf("3.Check Balance\n")
-	fmt.Println("4.Log out")
-	fmt.Printf("Exit program, type 0\n")
-	fmt.Printf("Enter your selection :")
+	fmt.Println("1.Create Account")
+	fmt.Println("2.Login")
+	fmt.Print("Enter your seletion : ")
 	fmt.Scan(&selection)
-
 	switch {
 	case selection == 1:
 		createAccount()
 	case selection == 2:
-		transaction()
-	case selection == 3:
-		balance()
-	case selection == 4:
-		main()
-	case selection == 0:
-		exitProgram()
-	default:
-		fmt.Println("Invalid")
-
-	}
-}
-
-func mainMenuAdmin() {
-	var selection int
-
-	fmt.Println("-------- Admin Main Menu -----------")
-	fmt.Println("1. View all list of users' account")
-	fmt.Println("Type 0 to exit the admin menu")
-	fmt.Printf("Enter your choice :")
-	fmt.Scan(&selection)
-	switch {
-	case selection == 1:
-		showAccount()
-	case selection == 0:
-		main()
+		loginpage()
 	}
 }
 
 type accountDetail struct {
-	date           string
+	Date           string
 	Account_Name   string
 	Account_Number int
 	Phone          string
-	balance        int
+	Password       string
+	Balance        uint32
 }
 
 func createAccount() {
 	var accountName string
 	var phone string
-	var balance int
+	var password string
+	var deposit uint32
+
 	min := 100000000000
 	max := 999999999999
 	randomNumber := rand.Intn(max-min) + min
@@ -103,105 +51,146 @@ func createAccount() {
 	fmt.Scan(&accountName)
 	fmt.Printf("Phone Number: ")
 	fmt.Scan(&phone)
+	fmt.Printf("Password: ")
+	fmt.Scan(&password)
 	fmt.Printf("Deposit: ")
-	fmt.Scan(&balance)
-	fmt.Println("")
+	fmt.Scan(&deposit)
 
 	a1 := (accountDetail{currentDate,
 		accountName,
 		randomNumber,
 		phone,
-		balance})
-
+		password,
+		deposit})
+	b, err := json.Marshal(a1)
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+	os.Stdout.Write(b)
 	listAccount = append(listAccount, a1)
-	fmt.Println("SAVED!")
-	mainMenuUser()
+
+	loginpage()
+}
+
+func loginpage() {
+	var password string
+	fmt.Printf("\nPhone no : ")
+	fmt.Scan(&phone)
+	fmt.Printf("Password : ")
+	fmt.Scan(&password)
+
+	for i := 0; i < len(listAccount); i++ {
+		if listAccount[i].Phone == phone && listAccount[i].Password == password {
+			transaction()
+		} else {
+			fmt.Println("Invalid password")
+		}
+	}
 }
 
 func transaction() {
-	var choice int
+	var selection int
+	fmt.Println("\n -------- User transaction Menu -----------")
+	fmt.Println("1.Deposit money")
+	fmt.Println("2.Withdraw money")
+	fmt.Println("3.Transfer money")
+	fmt.Println("4.Check balance")
+	fmt.Println("5.Logout")
+	fmt.Println("Exit program, type 0")
+	fmt.Printf("Enter your selection :")
+	fmt.Scan(&selection)
 
-	fmt.Printf("-----------------Transaction----------------\n")
-	fmt.Println("1. Deposit")
-	fmt.Println("2. Withdraw")
-	fmt.Println("3. Transfer money")
-	fmt.Printf("Enter your choice: ")
-	fmt.Scan(&choice)
 	switch {
-	case choice == 1:
+	case selection == 1:
 		deposit()
-	case choice == 2:
+	case selection == 2:
 		withdraw()
+	case selection == 3:
+		fmt.Println("No service yet")
+	case selection == 4:
+		balance()
+	case selection == 5:
+		main()
+	case selection == 0:
+		exitProgram()
+	default:
+		fmt.Println("Invalid")
+
 	}
 }
 
 func deposit() {
-	var number int
-	var money int
+	var money uint32
 
-	fmt.Printf("Write your account number : ")
-	fmt.Scan(&number)
 	fmt.Printf("Amount of money : ")
 	fmt.Scan(&money)
 	for i := 0; i < len(listAccount); i++ {
-		if number == listAccount[i].Account_Number {
-			listAccount[i].balance += money
+		if phone == listAccount[i].Phone {
+			listAccount[i].Balance += money
 			fmt.Println("Deposit success!")
 		} else {
 			fmt.Println("Invalid")
 		}
-		mainMenuUser()
+		transaction()
 	}
 }
 
 func withdraw() {
-	var number int
-	var money int
+	var money uint32
 
-	fmt.Printf("Write your account number : ")
-	fmt.Scan(&number)
 	fmt.Printf("Amount of money : ")
 	fmt.Scan(&money)
 	for i := 0; i < len(listAccount); i++ {
-		if number == listAccount[i].Account_Number {
-			if listAccount[i].Account_Number >= money {
-				listAccount[i].balance -= money
+		if phone == listAccount[i].Phone {
+			if listAccount[i].Balance >= money {
+				listAccount[i].Balance -= money
 			} else {
 				fmt.Println("Not enough money!")
 			}
 		}
-		mainMenuUser()
+		transaction()
 	}
 }
 
 func balance() {
-	var number int
-
-	fmt.Printf("Type your Account number : \n")
-	fmt.Scan(&number)
+	var number string
+	var choice int
 
 	for i := 0; i < len(listAccount); i++ {
-		if number == listAccount[i].Account_Number {
-			fmt.Println("Your balance is : ", listAccount[i].balance)
+		if phone == listAccount[i].Phone {
+
+			fmt.Println("Your balance is : ", listAccount[i].Balance)
 		}
 	}
-	mainMenuUser()
+	fmt.Printf("type 0 to exit : ")
+	fmt.Scan(&number)
+	if choice == 0 {
+		transaction()
+	}
 
 }
 
 func showAccount() {
+	var choice int
+
 	if len(listAccount) == 0 {
 		fmt.Println("There is no account")
 	} else {
 		fmt.Println("Date \t\t Name \t\t Account Number \t\t Phone \t\t\tBalance")
 		for i := 0; i < len(listAccount); i++ {
-			fmt.Println(listAccount[i].date, "\t", listAccount[i].Account_Name, "\t\t", listAccount[i].Account_Number, "\t\t\t", listAccount[i].Phone, "\t\t", listAccount[i].balance)
+			fmt.Println(listAccount[i].Date, "\t", listAccount[i].Account_Name, "\t\t", listAccount[i].Account_Number, "\t\t\t", listAccount[i].Phone, "\t\t", listAccount[i].Balance)
 
 		}
 		time.Sleep(1 * time.Second)
-		mainMenuAdmin()
+
+	}
+	fmt.Printf("Type 0 to exit : ")
+	fmt.Scan(&choice)
+	if choice == 0 {
+		transaction()
 	}
 }
+
 func exitProgram() {
 	os.Exit(3)
 }
