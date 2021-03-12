4519e8d645764e99060a957fdea4f14899e744cc
Elf <elf.manutd@gmail.com>
2021-03-12 17:31:37 +0700

    Update loadTest.go

--- loadTest/loadTest.go
+++ loadTest/loadTest.go
@@ -17,24 +17,25 @@ type clientData struct {
 	money          int64
 }
 
+func connHandled(c chan int, j int)
 // func connHandled(c chan int, j int, jdata []byte) {
-func connHandled(j int, jdata []byte) {
+// func connHandled(j int, jdata []byte) {
 	// Addr := "localhost:7777"
-	// Addr := "10.36.15.220:9091"
-	// // msg := "hello world\n"
-	// conn, err := net.Dial("tcp", Addr)
-	// if err != nil {
-	// 	log.Println("Dial failed for address: " + Addr)
-	// }
-	// conn.Write([]byte(msg))
-	// conn.Write(jdata)
-	var m clientData
-	log.Print(jdata)
-	err := json.Unmarshal(jdata, &m)
+	Addr := "10.36.15.220:9091"
+	// msg := "hello world\n"
+	conn, err := net.Dial("tcp", Addr)
 	if err != nil {
-		log.Print("no json found")
+		log.Println("Dial failed for address: " + Addr)
 	}
-	log.Print(m)
+	conn.Write([]byte(msg))
+	conn.Write(jdata)
+	// var m clientData
+	// log.Print(jdata)
+	// err := json.Unmarshal(jdata, &m)
+	// if err != nil {
+	// 	log.Print("no json found")
+	// }
+	// log.Print(m)
 	log.Print("print somthing " + strconv.Itoa(j))
 	// defer conn.Close()
 	// c <- 0
@@ -44,8 +45,8 @@ func main() {
 	start := time.Now()
 	dataType := [5]string{"transfer", "withdraw", "deposit", "buystock", "sellstock"}
 	n := 5
-	var b clientData
-	// c := make(chan int)
+	// var b clientData
+	c := make(chan int)
 	// dataType[rand.Intn(5)]
 	// log.Print("hello")
 	for i := 0; i < n; i++ {
@@ -60,20 +61,21 @@ func main() {
 		if err != nil {
 			log.Println("json error")
 		}
-		fmt.Println(string(jsonData))
-		err = json.Unmarshal(jsonData, &b)
-		if err != nil {
-			log.Print("json not found")
-		}
-		// log.Print(b)
-		log.Print(jsonData)
+		// fmt.Println(string(jsonData))
+		// err = json.Unmarshal(jsonData, &b)
+		// if err != nil {
+		// 	log.Print("json not found")
+		// }
+		// // log.Print(b)
+		// log.Print(jsonData)
 		// jsonData == []byte()
 		// log.Print(k)
 		// log.Print(i)
+		go connHandled(c, i)
 		// go connHandled(c, i, jsonData)
 		// connHandled(i, jsonData)
 	}
 	elasped := time.Since(start)
 	log.Print(elasped)
-	// <-c
+	<-c
 }
