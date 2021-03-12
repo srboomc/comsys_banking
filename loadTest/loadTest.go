8d97d046634f5f34b60edeae708ddce58874a265
Elf <elf.manutd@gmail.com>
2021-03-12 18:00:02 +0700

    Update loadTest.go

--- loadTest/loadTest.go
+++ loadTest/loadTest.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"math/rand"
+	"net"
 	"strconv"
 	"time"
 )
@@ -17,18 +18,18 @@ type clientData struct {
 	money          int64
 }
 
-func connHandled(c chan int, j int)
-// func connHandled(c chan int, j int, jdata []byte) {
-// func connHandled(j int, jdata []byte) {
-	// Addr := "localhost:7777"
-	Addr := "10.36.15.220:9091"
-	// msg := "hello world\n"
+func connHandled(c chan int, j int) {
+	// func connHandled(c chan int, j int, jdata []byte) {
+	// func connHandled(j int, jdata []byte) {
+	Addr := "localhost:9091"
+	// Addr := "10.36.15.220:9091"
+	msg := "hello world\n"
 	conn, err := net.Dial("tcp", Addr)
 	if err != nil {
 		log.Println("Dial failed for address: " + Addr)
 	}
 	conn.Write([]byte(msg))
-	conn.Write(jdata)
+	// conn.Write(jdata)
 	// var m clientData
 	// log.Print(jdata)
 	// err := json.Unmarshal(jdata, &m)
@@ -37,8 +38,8 @@ func connHandled(c chan int, j int)
 	// }
 	// log.Print(m)
 	log.Print("print somthing " + strconv.Itoa(j))
-	// defer conn.Close()
-	// c <- 0
+	defer conn.Close()
+	c <- 0
 }
 
 func main() {
@@ -52,16 +53,16 @@ func main() {
 	for i := 0; i < n; i++ {
 		v := dataType[rand.Intn(5)]
 		// log.Print(v)
-		m := &clientData{v, 123455, 123456, 200}
+		m := clientData{v, 123455, 123456, 200}
 		if v != "transfer" {
 			m.recieverAcc = 0
 		}
-		log.Print(m)
-		jsonData, err := json.Marshal(m)
-		if err != nil {
-			log.Println("json error")
-		}
-		// fmt.Println(string(jsonData))
+		// log.Print(m)
+		jsonData, _ := json.Marshal(m)
+		// if err != nil {
+		// 	log.Println("json error")
+		// }
+		fmt.Println(string(jsonData))
 		// err = json.Unmarshal(jsonData, &b)
 		// if err != nil {
 		// 	log.Print("json not found")
